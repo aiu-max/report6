a19ad014f67ce3818523123ab5fce7b909f398c4
shiotsuki ryusei <e195761@ie.u-ryukyu.ac.jp>
2020-02-04 02:20:05 +0900

    report6-1

--- src/jp/ac/uryukyu/ie/e195761/Hit.java
+++ src/jp/ac/uryukyu/ie/e195761/Hit.java
@@ -6,46 +6,54 @@ import java.util.regex.Pattern;
 
 class Hit  {
     public void hit(Character character) {
-        int i = 0;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("数値を入力してください。");
-        String num = scanner.next();
-        String array[] = new String[num.length()];
-        for (i = 0; i < array.length; i++) {
-            array[i] = num.substring(i, i + 1);
-        }
-
-        System.out.println(character.player + "が入力した数値は " + num + " です。");
-
-        String array2[] = new String[character.number.length()];
-        for (i = 0; i < character.number.length(); i++) {
-            array2[i] = character.number.substring(i, i + 1);
-        }
-
-        int ito, bi;
-        ito = 0;
-        bi = 0;
-        for (i = 0; i < num.length(); i++) {
-
-            for (int e = 0; e < character.number.length(); e++) {
-                if (array[i].equals(array2[e]) && i == e) {
-                    System.out.println("1イート");
-                    ito += 1;
-                } else if (array[i].equals(array2[e])) {
-                    System.out.println("1バイト");
-                    bi += 1;
+        while (true) {
+            int i = 0;
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("数値を入力してください。");
+            String num = scanner.next();
+            Pattern p = Pattern.compile("^\\d{3}$");
+            Matcher m = p.matcher(num);
+            if(m.find() == true) {
+                String array[] = new String[num.length()];
+                for (i = 0; i < array.length; i++) {
+                    array[i] = num.substring(i, i + 1);
                 }
 
+                System.out.println(character.player + "が入力した数値は " + num + " です。");
+
+                String array2[] = new String[character.number.length()];
+                for (i = 0; i < character.number.length(); i++) {
+                    array2[i] = character.number.substring(i, i + 1);
+                }
+
+                int ito, bi;
+                ito = 0;
+                bi = 0;
+                for (i = 0; i < num.length(); i++) {
+
+                    for (int e = 0; e < character.number.length(); e++) {
+                        if (array[i].equals(array2[e]) && i == e) {
+                            System.out.println("1イート");
+                            ito += 1;
+                        } else if (array[i].equals(array2[e])) {
+                            System.out.println("1バイト");
+                            bi += 1;
+                        }
+
+                    }
+                }
+                String a = Integer.toString(ito);
+                String b = Integer.toString(bi);
+                System.out.println("結果は" + a + "イート" + b + "バイト");
+                if (a.equals("3")) {
+                    System.out.println(character.player + "の勝利です");
+
+                }
             }
-        }
-        String a = Integer.toString(ito);
-        String b = Integer.toString(bi);
-        System.out.println("結果は" + a + "イート" + b + "バイト");
-        if (a.equals("3")) {
-            System.out.println(character.player + "の勝利です");
-
-        }
-
-
+            else {
+                System.out.println("3桁の数字で入力してください");
+                continue;
+            }
+            }
     }
 }
